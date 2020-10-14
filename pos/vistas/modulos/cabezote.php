3b2a7a84296eaea635366c15a0a22768a46947a4
EverSaul1 <ever.saul.2017@gmail.com>
2020-10-14 12:44:51 -0500

    img

--- pos/vistas/modulos/cabezote.php
+++ pos/vistas/modulos/cabezote.php
@@ -8,7 +8,7 @@
 		<!-- logo mini -->
 		<span class="logo-mini">
 			
-			<img src="vistas/img/plantilla/icono-blanco.png" class="img-responsive" style="padding:10px">
+			<img src="vistas/img/plantilla/icon4.png" class="img-responsive" style="padding:10px">
 
 		</span>
 
@@ -16,7 +16,7 @@
 
 		<span class="logo-lg">
 			
-			<img src="vistas/img/plantilla/logo-blanco-lineal.png" class="img-responsive" style="padding:10px 0px">
+			<img src="vistas/img/plantilla/logotipo2.png" class="img-responsive" style="padding:10px 0px">
 
 		</span>
 
--- pos/vistas/plantilla.php
+++ pos/vistas/plantilla.php
@@ -11,7 +11,7 @@ session_start();
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-  <title>Inventory System</title>
+  <title>REDSTORE</title>
 
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
